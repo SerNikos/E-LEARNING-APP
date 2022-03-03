dacb1cd3675fadf94dc915505870714835c8cc9d
Alexander Margarhs <alexander.margarhs@gmail.com>
2022-03-03 16:38:32 +0200

    Singleton pattern.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,16 @@ public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
 
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance is null)
+                Debug.LogError("Game Manager is NULL");
+
+            return instance;
+        }
+    }
    
     void Awake()
     {
