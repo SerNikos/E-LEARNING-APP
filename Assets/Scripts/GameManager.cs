fb4bf40d69b1b0771931c9e195f2b607719cfc2e
SerNikos <78097143+SerNikos@users.noreply.github.com>
2022-03-03 16:24:40 +0200

    stuff happened
    indeed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager instance;
+
+   
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+    public void ChangeSceneSelectChapter(int number)
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
