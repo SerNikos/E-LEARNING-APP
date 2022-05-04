0a23718395cbd1493ea2cccff9d7e3589bd17fbf
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2022-05-04 16:53:50 +0300

    fixed UI, replace text to questions

--- Assets/Scripts/SelectQuestions1.cs
+++ Assets/Scripts/SelectQuestions1.cs
@@ -2,18 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random=System.Random;
+using TMPro;
+using System;
 
 public class SelectQuestions1 : MonoBehaviour
 {
     private List<List<string>> data;
+
+    [SerializeField]
+    private TMP_Text Question1,Question2,Question3,Question4,Question5; 
+
+    void Awake() {
+        Question1 = GameObject.Find("Canvas/Panel/Scroll View/Viewport/Content/Grid/Question1").GetComponent<TMP_Text>();
+        Question2 = GameObject.Find("Canvas/Panel/Scroll View/Viewport/Content/Grid/Question2").GetComponent<TMP_Text>();
+        Question3 = GameObject.Find("Canvas/Panel/Scroll View/Viewport/Content/Grid/Question3").GetComponent<TMP_Text>();
+        Question4 = GameObject.Find("Canvas/Panel/Scroll View/Viewport/Content/Grid/Question4").GetComponent<TMP_Text>();
+        Question5 = GameObject.Find("Canvas/Panel/Scroll View/Viewport/Content/Grid/Question5").GetComponent<TMP_Text>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         data = RealTimeDatabase.Questions_1;
 
-        //QuestionSelectorTrueFalse();
-        //QuestionSelectorMultipleChoice();
+        QuestionSelectorTrueFalse();
+        QuestionSelectorMultipleChoice();
     }
 
     public void QuestionSelectorTrueFalse(){
@@ -28,9 +41,12 @@ public class SelectQuestions1 : MonoBehaviour
         }
 
         for (int i = 0; i<data.Count; i++){
-            if (data[i][0] == firstQ || data[i][0] == secondQ){
+            if (data[i][0] == firstQ){
                 //function to replace text on test
-                //Debug.Log(data[i][0]);
+                Question1.text = data[i][2];
+            }
+            else if (data[i][0] == secondQ) {
+                Question2.text = data[i][2];
             }
         }
     }
@@ -53,9 +69,15 @@ public class SelectQuestions1 : MonoBehaviour
         }
 
         for (int i = 0; i<data.Count; i++){
-            if (data[i][0] == firstQ || data[i][0] == secondQ || data[i][0] == thirdQ){
+            if (data[i][0] == firstQ ){
                 //function to replace text on test
-                Debug.Log(data[i][0]);
+                Question3.text = data[i][2];
+            }
+            else if (data[i][0] == secondQ){
+                Question4.text = data[i][2];
+            }
+            else if (data[i][0] == thirdQ) {
+                Question5.text = data[i][2];
             }
         }
     }
