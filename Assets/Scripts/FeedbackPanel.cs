a16032eea6ead206f6e36a945de1a0d3a5bcd882
SerNikos <78097143+SerNikos@users.noreply.github.com>
2022-05-14 19:21:27 +0300

    lil fix feedback panel script

--- Assets/Scripts/FeedbackPanel.cs
+++ Assets/Scripts/FeedbackPanel.cs
@@ -12,10 +12,18 @@ public class FeedbackPanel : MonoBehaviour
         //logic here we need to find the grade varable from the database
         if (grade >= 50)
         {
+            if (badpanel)
+            {
+                badpanel.SetActive(false);
+            }
             goodpanel.SetActive(true);
         }
         else
         {
+            if (goodpanel)
+            {
+                goodpanel.SetActive(false);
+            }
             badpanel.SetActive(true);
         }
     }
