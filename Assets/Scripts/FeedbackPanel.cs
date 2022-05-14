fe28afb03cd640ab785f3b77e39c5270bb368f5a
SerNikos <78097143+SerNikos@users.noreply.github.com>
2022-05-14 19:18:05 +0300

    grade script for feedback panel

---
+++ Assets/Scripts/FeedbackPanel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackPanel : MonoBehaviour
+{
+    public int grade;
+    public GameObject goodpanel, badpanel;
+  
+    void Start()
+    {
+        //logic here we need to find the grade varable from the database
+        if (grade > 50)
+        {
+            goodpanel.SetActive(true);
+        }
+        else
+        {
+            badpanel.SetActive(true);
+        }
+    }
+
+}
